17e0e92b3191db1f77bb65baeeba1692ce98dc76
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2025-12-17 17:58:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,24 +4,28 @@ class Config:
     # Telegram Settings
     TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
     TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', '')
-    
+
     # CoinDCX Settings
     COINDCX_BASE_URL = "https://public.coindcx.com"
-    MARKETS = os.environ.get('COINDCX_MARKETS', 'B-BTC_USDT,B-ETH_USDT,B-SOL_USDT,B-XRP_USDT,B-MATIC_USDT,B-ADA_USDT').split(',')
-    
-    # Trading Parameters
-    CANDLE_INTERVAL = os.environ.get('CANDLE_INTERVAL', '15m')
-    MIN_SIGNAL_SCORE = int(os.environ.get('MIN_SIGNAL_SCORE', '75'))
-    CHECK_INTERVAL_MINUTES = int(os.environ.get('CHECK_INTERVAL_MINUTES', '15'))
-    COOLDOWN_MINUTES = int(os.environ.get('COOLDOWN_MINUTES', '30'))
-    MAX_SIGNALS_PER_DAY = int(os.environ.get('MAX_SIGNALS_PER_DAY', '10'))
-    
+    MARKETS = os.environ.get(
+        'COINDCX_MARKETS',
+        'B-BTC_USDT,B-ETH_USDT,B-SOL_USDT,B-XRP_USDT,B-MATIC_USDT,B-ADA_USDT'
+    ).split(',')
+
+    # 🔥 UPDATED TRADING PARAMETERS
+    CANDLE_INTERVAL = '5m'
+    CHECK_INTERVAL_MINUTES = 5
+
+    MIN_SIGNAL_SCORE = 70
+    COOLDOWN_MINUTES = 30
+    MAX_SIGNALS_PER_DAY = 12
+
     # Risk Management
     ATR_SL_MULTIPLIER = 1.5
     ATR_TP1_MULTIPLIER = 2.0
     ATR_TP2_MULTIPLIER = 3.5
     MIN_RR_RATIO = 1.5
-    
+
     # Technical Settings
     RSI_PERIOD = 14
     RSI_OVERBOUGHT = 70
@@ -35,18 +39,18 @@ class Config:
     ADX_PERIOD = 14
     ADX_STRONG_TREND = 25
     ATR_PERIOD = 14
-    
-    # Multi-Timeframe
-    MTF_TIMEFRAMES = ['5m', '15m', '1h']
-    
+
+    # ❌ Fake MTF removed (not used)
+    # MTF_TIMEFRAMES = ['5m', '15m', '1h']
+
     # Volume Analysis
     VOLUME_SPIKE_THRESHOLD = 1.5
     POC_LOOKBACK = 50
-    
+
     # Smart Money Concepts
     FVG_MIN_SIZE = 0.002
     LIQUIDITY_GRAB_LOOKBACK = 20
-    
+
     # Signal Scoring Weights
     SCORE_WEIGHTS = {
         'trend_alignment': 20,
