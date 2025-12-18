1e408599786a8ae0d8de269f317b8e137873aa9c
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2025-12-18 15:38:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,38 +1,37 @@
 import os
 
 class Config:
-    # Telegram
     TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
     TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', '')
     
-    # CoinDCX
     COINDCX_BASE_URL = "https://public.coindcx.com"
     MARKETS = os.environ.get('COINDCX_MARKETS', 'B-BTC_USDT,B-ETH_USDT,B-SOL_USDT,B-XRP_USDT,B-MATIC_USDT,B-ADA_USDT,B-DOGE_USDT,B-DOT_USDT,B-LTC_USDT,B-LINK_USDT,B-UNI_USDT,B-AVAX_USDT,B-ATOM_USDT,B-TRX_USDT,B-SHIB_USDT').split(',')
     
-    # Trading (BALANCED)
+    # Trading
     CANDLE_INTERVAL = os.environ.get('CANDLE_INTERVAL', '15m')
-    MIN_SIGNAL_SCORE = int(os.environ.get('MIN_SIGNAL_SCORE', '60'))
+    MIN_SIGNAL_SCORE = int(os.environ.get('MIN_SIGNAL_SCORE', '68'))
     CHECK_INTERVAL_MINUTES = int(os.environ.get('CHECK_INTERVAL_MINUTES', '5'))
-    COOLDOWN_MINUTES = int(os.environ.get('COOLDOWN_MINUTES', '20'))
+    COOLDOWN_MINUTES = int(os.environ.get('COOLDOWN_MINUTES', '25'))
     MAX_SIGNALS_PER_DAY = int(os.environ.get('MAX_SIGNALS_PER_DAY', '999'))
     
-    # Risk Management
+    # Risk
     ATR_SL_MULTIPLIER = 1.5
     ATR_TP1_MULTIPLIER = 2.0
     ATR_TP2_MULTIPLIER = 3.5
-    MIN_RR_RATIO = 1.3
+    MIN_RR_RATIO = 1.4
     
     # Technical
     RSI_PERIOD = 14
-    RSI_OVERBOUGHT = 70
-    RSI_OVERSOLD = 30
     EMA_FAST = 9
     EMA_SLOW = 21
     ADX_PERIOD = 14
-    ADX_STRONG = 20
     ATR_PERIOD = 14
     
-    # Regime Filters (RELAXED)
-    BLOCK_RANGING_MARKETS = False
-    BLOCK_HIGH_VOLATILITY = False
-    MAX_VOLATILITY_PERCENT = 8.0
+    # SMART FILTERS (HARD BLOCKS)
+    BLOCK_RANGING_IF_SCORE_BELOW = 70
+    BLOCK_VOLATILE_IF_SCORE_BELOW = 75
+    MIN_ADX_FOR_RANGING_OVERRIDE = 18
+    
+    # Quality Tiers
+    HIGH_QUALITY_THRESHOLD = 75
+    MEDIUM_QUALITY_THRESHOLD = 68
