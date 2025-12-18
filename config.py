39ca7e80f02d16cdb7fc19339e9f6bb2f55e34a7
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2025-12-18 15:27:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ class Config:
     COINDCX_BASE_URL = "https://public.coindcx.com"
     MARKETS = os.environ.get('COINDCX_MARKETS', 'B-BTC_USDT,B-ETH_USDT,B-SOL_USDT,B-XRP_USDT,B-MATIC_USDT,B-ADA_USDT,B-DOGE_USDT,B-DOT_USDT,B-LTC_USDT,B-LINK_USDT,B-UNI_USDT,B-AVAX_USDT,B-ATOM_USDT,B-TRX_USDT,B-SHIB_USDT').split(',')
     
-    # Trading
+    # Trading (BALANCED)
     CANDLE_INTERVAL = os.environ.get('CANDLE_INTERVAL', '15m')
-    MIN_SIGNAL_SCORE = int(os.environ.get('MIN_SIGNAL_SCORE', '75'))
+    MIN_SIGNAL_SCORE = int(os.environ.get('MIN_SIGNAL_SCORE', '60'))
     CHECK_INTERVAL_MINUTES = int(os.environ.get('CHECK_INTERVAL_MINUTES', '5'))
-    COOLDOWN_MINUTES = int(os.environ.get('COOLDOWN_MINUTES', '30'))
+    COOLDOWN_MINUTES = int(os.environ.get('COOLDOWN_MINUTES', '20'))
     MAX_SIGNALS_PER_DAY = int(os.environ.get('MAX_SIGNALS_PER_DAY', '999'))
     
     # Risk Management
     ATR_SL_MULTIPLIER = 1.5
     ATR_TP1_MULTIPLIER = 2.0
     ATR_TP2_MULTIPLIER = 3.5
-    MIN_RR_RATIO = 1.5
+    MIN_RR_RATIO = 1.3
     
     # Technical
     RSI_PERIOD = 14
@@ -29,10 +29,10 @@ class Config:
     EMA_FAST = 9
     EMA_SLOW = 21
     ADX_PERIOD = 14
-    ADX_STRONG = 25
+    ADX_STRONG = 20
     ATR_PERIOD = 14
     
-    # Regime Filters
-    BLOCK_RANGING_MARKETS = True
-    BLOCK_HIGH_VOLATILITY = True
-    MAX_VOLATILITY_PERCENT = 5.0
+    # Regime Filters (RELAXED)
+    BLOCK_RANGING_MARKETS = False
+    BLOCK_HIGH_VOLATILITY = False
+    MAX_VOLATILITY_PERCENT = 8.0
