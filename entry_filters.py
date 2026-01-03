54c86f5ec0543847b65fedef871331eb7b5be24d
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2026-01-03 20:52:05 +0530

    Update entry_filters.py

--- entry_filters.py
+++ entry_filters.py
@@ -2,7 +2,7 @@
 entry_filters.py - Entry Timing Filters & Multi-Confirmation System
 """
 
-from typing import Tuple, List
+from typing import Dict, Tuple
 import pandas as pd
 from indicators import Indicators
 from coindcx_api import CoinDCXAPI
